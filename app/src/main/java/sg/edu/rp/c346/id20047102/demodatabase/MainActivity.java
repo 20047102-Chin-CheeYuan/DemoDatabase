378bb00025800dcd1cd505997df9ccb238f5c91b
CheeYuan <20047102@myrp.edu.sg>
2022-07-07 12:55:15 +0800

    Added Database to the Codes

--- app/src/main/java/sg/edu/rp/c346/id20047102/demodatabase/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20047102/demodatabase/MainActivity.java
@@ -3,12 +3,72 @@ package sg.edu.rp.c346.id20047102.demodatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert, btnGetTasks;
+    TextView tvResults;
+    EditText etDesc;
+    EditText etDate;
+    ListView lvResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnGetTasks = findViewById(R.id.btnGetTasks);
+        tvResults = findViewById(R.id.tvResults);
+        etDesc = findViewById(R.id.editTextDesc);
+        etDate = findViewById(R.id.editTextDate);
+        lvResults = findViewById(R.id.lvResults);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask(etDesc.getText().toString(), etDate.getText().toString());
+
+            }
+        });
+
+        btnGetTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                db.close();
+
+                String txt = "";
+                for (int i = 0; i < data.size(); i++) {
+                    Log.d("Database Content", i + ". " + data.get(i));
+                    txt += i + ". " + data.get(i) + "\n";
+                }
+//                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+//                lvResults.setAdapter(adapter);
+
+                tvResults.setText(txt);
+            }
+        });
+
+
     }
 }
