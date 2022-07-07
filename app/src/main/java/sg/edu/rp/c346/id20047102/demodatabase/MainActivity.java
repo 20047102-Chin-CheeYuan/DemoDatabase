5c2d667f292d4b554528a2ce0be6dbf543c2e81d
CheeYuan <20047102@myrp.edu.sg>
2022-07-07 15:12:17 +0800

    Changed out TextView to ListView to display database data

--- app/src/main/java/sg/edu/rp/c346/id20047102/demodatabase/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20047102/demodatabase/MainActivity.java
@@ -3,23 +3,22 @@ package sg.edu.rp.c346.id20047102.demodatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnInsert, btnGetTasks;
-    TextView tvResults;
     EditText etDesc;
     EditText etDate;
     ListView lvResults;
+    ArrayAdapter<Task> aa;
+    ArrayList<Task> alTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
         btnInsert = findViewById(R.id.btnInsert);
         btnGetTasks = findViewById(R.id.btnGetTasks);
-        tvResults = findViewById(R.id.tvResults);
         etDesc = findViewById(R.id.editTextDesc);
         etDate = findViewById(R.id.editTextDate);
-        lvResults = findViewById(R.id.lvResults);
+        lvResults = findViewById(R.id.lv);
 
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,20 +50,12 @@ public class MainActivity extends AppCompatActivity {
                 // Create the DBHelper object, passing in the
                 // activity's Context
                 DBHelper db = new DBHelper(MainActivity.this);
-
-                // Insert a task
-                ArrayList<String> data = db.getTaskContent();
                 db.close();
 
-                String txt = "";
-                for (int i = 0; i < data.size(); i++) {
-                    Log.d("Database Content", i + ". " + data.get(i));
-                    txt += i + ". " + data.get(i) + "\n";
-                }
-//                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
-//                lvResults.setAdapter(adapter);
 
-                tvResults.setText(txt);
+                alTask = db.getTasks();
+                aa = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, alTask);
+                lvResults.setAdapter(aa);
             }
         });
 
